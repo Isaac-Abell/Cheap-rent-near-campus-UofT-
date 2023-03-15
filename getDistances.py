96bebb496dab439437ebb61f2edce274cfa4e1de
Isaac Abell <83926340+Isaac-Abell@users.noreply.github.com>
2023-03-15 14:24:11 -0400

    Add files via upload

--- getDistances.py
+++ getDistances.py
@@ -5,12 +5,13 @@ from gethouses import get_houses
 API = open("key.txt", "r")
 APIkey = API.read()
 
-MAX_MINUTES = 20
-MAX_PRICE = 2500
+MAX_MINUTES = 15
+MAX_PRICE = 2700
+MIN_ROOMS = 0
 
 gmaps = googlemaps.Client(key=APIkey)
 
-houses = get_houses(MAX_PRICE)
+houses = get_houses(MAX_PRICE, MIN_ROOMS)
 
 end = "31 King's College Circle"
 
@@ -22,18 +23,20 @@ for house in houses:
     if "hour" not in str(distance) and int(str(distance[:2]).strip()) <= MAX_MINUTES:
         other_stuff = {}
         other_stuff["address"] = house[0]
-        other_stuff["link"] = house[2]
-        other_stuff["distance"] = distance
         other_stuff["price"] = house[1]
+        other_stuff["beds"] = house[2]
+        other_stuff["link"] = house[3]
+        other_stuff["distance"] = distance
         good_distance.append(other_stuff)
     
 address = [x["address"] for x in good_distance]
 link = [x["link"] for x in good_distance]
 distance = [x["distance"] for x in good_distance]
 price = [x["price"] for x in good_distance]
+beds = [x["beds"] for x in good_distance]
 
-columns = ["address", "link", "distance", "price"]
-dataframe = pd.DataFrame(list(zip(address, link, distance, price)), columns = columns)
+columns = ["address", "link", "distance", "price", "beds"]
+dataframe = pd.DataFrame(list(zip(address, link, distance, price, beds)), columns = columns)
 
 dataframe.to_excel("housing.xlsx")
 
--- gethouses.py
+++ gethouses.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 import math
 
-def get_houses(max: int, pages_checked = None):
+def get_houses(max_price: int, min_beds: int, pages_checked = None):
 
     browser = webdriver.Chrome()
 
@@ -12,14 +12,25 @@ def get_houses(max: int, pages_checked = None):
     link = []
     prices = []
     locations = []
+    beds = []
     if pages_checked is None:
         pages_checked = browser.find_element(By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/p[1]/strong[1]").text
         pages_checked = int(pages_checked[0:pages_checked.index(" ")])
         pages_checked = math.ceil(pages_checked / 10)
 
     for i in range(1, pages_checked):
+        temp_beds = []
+        temp_prices = []
+        temp_locations = []
+        temp_link = []
+
+        for j in browser.find_elements(By.CLASS_NAME, "listing-card__main-features"):
+            bed = j.text
+            bed = bed[0:3].strip("B").strip("-").strip()
+            temp_beds.append(float(bed))
+
         for j in browser.find_elements(By.CLASS_NAME, "listing-card__details-link"):
-            link.append(j.get_attribute("href"))
+            temp_link.append(j.get_attribute("href"))
 
         for j in browser.find_elements(By.CLASS_NAME, "listing-card__price"):
             price = j.text
@@ -27,19 +38,23 @@ def get_houses(max: int, pages_checked = None):
             price = price[price.index("$")+1:].strip()
             if "-" in price:
                 price = price[0:price.index("-") - 1]
-
-            prices.append(int(price))
+            temp_prices.append(int(price))
 
         for j in browser.find_elements(By.CLASS_NAME, "listing-card__title"):
-            locations.append(j.text)
+            temp_locations.append(j.text)
 
-        for k in prices:
-            if k > max:
-                p = prices.index(k)
-                prices.pop(p)
-                locations.pop(p)
-                link.pop(p)
-        
+        bad_indexes = []
+        for k in range(0, len(temp_prices)):
+            if temp_prices[k] > max_price or temp_beds[k] < min_beds:
+                bad_indexes.append(k)
+
+        for p in range(0, len(temp_prices)):
+            if p not in bad_indexes:
+                prices.append(temp_prices[p])
+                link.append(temp_link[p])
+                locations.append(temp_locations[p])
+                beds.append(temp_beds[p])
+
         if i == 1:
             browser.get("https://rentals.ca/toronto" + "?p=" + str(i+1))
             time.sleep(30)
@@ -50,9 +65,8 @@ def get_houses(max: int, pages_checked = None):
     info = []
 
     for i in range(0, len(prices)):
-        info.append([locations[i], prices[i], link[i]])
+        info.append([locations[i], prices[i], beds[i], link[i]])
 
     info.sort(key = lambda x: x[1])
-
+    
     return info
-
