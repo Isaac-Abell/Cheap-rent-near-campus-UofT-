e252ed0d284b484fb8c62c61f248ac710895ab05
Isaac Abell <83926340+Isaac-Abell@users.noreply.github.com>
2023-08-14 14:58:18 -0400

    Update getDistances.py

--- getDistances.py
+++ getDistances.py
@@ -23,30 +23,27 @@ def find-cheap-rent(min, price, rooms):
     
     end = "31 King's College Circle"
     
-    good_distance = []
-
-    // Uses the google matrix api to find the distance between the houses and the school
+    address = []
+    price = []
+    beds = []
+    link = []
+    distance = []
+    
     for house in houses:
         start = house[0]
         distance = gmaps.distance_matrix(start, end, mode='walking')["rows"][0]["elements"][0]["duration"]["text"]
         if "hour" not in str(distance) and int(str(distance[:2]).strip()) <= MAX_MINUTES:
             other_stuff = {}
-            other_stuff["address"] = house[0]
-            other_stuff["price"] = house[1]
-            other_stuff["beds"] = house[2]
-            other_stuff["link"] = house[3]
-            other_stuff["distance"] = distance
-            good_distance.append(other_stuff)
+            address.append(house[0])
+            price.append(house[1])
+            beds.append(house[2])
+            link.append(house[3])
+            distance.append(distance)
         
-    address = [x["address"] for x in good_distance]
-    link = [x["link"] for x in good_distance]
-    distance = [x["distance"] for x in good_distance]
-    price = [x["price"] for x in good_distance]
-    beds = [x["beds"] for x in good_distance]
+    
     
     columns = ["address", "link", "distance", "price", "beds"]
     dataframe = pd.DataFrame(list(zip(address, link, distance, price, beds)), columns = columns)
-    
     dataframe.to_excel("housing.xlsx")
     return(dataframe)
 
